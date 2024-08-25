04a6af4a0bd400718fa103fdff7d75d8c9006e47
natbioufpr@gmail.com <renilsonlisboajunior@gmail.com>
2024-08-25 13:58:23 -0300

    modified:   Project.toml 	modified:   src/Functions.jl

--- src/Functions.jl
+++ src/Functions.jl
@@ -1,31 +1,19 @@
 module Functions
 
+  using Statistics
+
     export DescrtiptiveStatistics
 
     function DescriptiveStatistics(x)
-        # Número de dados presentes em "X"
-        n= length(x)
-
-        Média = sum(x)/n
-
+        n = length(x)
+        Media = sum(x)/n
         pm = (maximum(x)+minimum(x))/2
-
-        Mediana = if iseven(n)
-                    median_value = (x[n ÷ 2] + x[n ÷ 2 + 1]) / 2
-                  else
-                    median_value = x[Int(n ÷ 2) + 1]
-                  end
-
+        mediana = median(x)
         Moda=mode(x)
-
-        AT=maximum(x)-minimum(x)
-
-        s²=var(x)
-
-        s=std(x)
-
-        cv=s/Média*100
-        
+        AT = maximum(x)-minimum(x)
+        s² = var(x)
+        s = std(x)
+        cv= (s/Media)*100
     end
 
 end
